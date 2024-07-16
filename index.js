c36e9e35717faf9d123d3c66dd52b81fbb56362c
thirumurugan sivalingam <thiru@synlian.net>
2024-07-16 12:17:09 +0530

    completed documentation

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ const client = new TonClient({
 
 
 const func = async ( ) => {
+
+  // mnemonic generated
     let mnemonics = await mnemonicNew();
 
     console.log("mne", mnemonics);
 
+    // replaced with my mnemonics from TON Keeper app
     mnemonics = [ 'seek',
     'decade',
     'midnight',
@@ -41,58 +44,79 @@ const func = async ( ) => {
 
     console.log("mne", mnemonics);
     
+    // generating keypair from mnemonics
 let keyPair = await mnemonicToPrivateKey(mnemonics);
 
+//logging the keypair
 console.log("pair",keyPair);
 
 
 // Create wallet contract
 let workchain = 0; // Usually you need a workchain 0
+
+// creating a wallet instance
 let wallet = WalletContractV4.create({ workchain, publicKey: keyPair.publicKey });
 
 console.log("wallet",wallet);
 
+
+// all the address are considered as contracts, so we need a contract instance to interact with any accounts
 let contract = client.open(wallet);
 
+// logging the contract instance
 console.log("contract",contract);
 
 
 // Get balance
 let balance = await contract.getBalance();
 
+//logging the balance
 console.log("balance",balance);
 
 
-// Create a transfer
+// Get Wallet Seqno
 let seqno = await contract.getSeqno();
 
+// logging the seqno
+console.log("seqno", seqno);
 
-// let transfer = await contract.createTransfer({
-//   seqno,
-//   secretKey: keyPair.secretKey,
-//   messages: [internal({
-//     value: '1.5',
-//     to: 'EQB_AmFKfZ3yIKdc7q7rKWV2gUKxTvfzQRwBoqLw98D9BiMp',
-//     body: 'Hello world',
-//   })]
-// });
+let transfer = await contract.createTransfer({
+  seqno,
+  secretKey: keyPair.secretKey,
+  messages: [internal({
+    value: '1.5',
+    to: 'EQB_AmFKfZ3yIKdc7q7rKWV2gUKxTvfzQRwBoqLw98D9BiMp',
+    body: 'Hello world',
+  })]
+});
 
-// console.log("transfer",transfer);
+
+
+console.log("transfer",transfer);
+
+let sendd = await contract.sender(keyPair.secretKey)
+console.log("sendd", sendd);
+
+
+// sendd.send({    value: '0.5',
+// to: 'EQB_AmFKfZ3yIKdc7q7rKWV2gUKxTvfzQRwBoqLw98D9BiMp',
+
+// })
 
 try {
-  const tx = await contract.sendTransfer({
-    messages:  [internal({
-      value: '1',
-      to: 'EQB_AmFKfZ3yIKdc7q7rKWV2gUKxTvfzQRwBoqLw98D9BiMp',
-      body: 'Hello world',
-    })],
-    seqno:seqno, secretKey:  keyPair.secretKey,
+  // const tx = await contract.sendTransfer({
+  //   messages:  [internal({
+  //     value: '1',
+  //     to: 'EQB_AmFKfZ3yIKdc7q7rKWV2gUKxTvfzQRwBoqLw98D9BiMp',
+  //     body: 'Hello world',
+  //   })],
+  //   seqno:seqno, secretKey:  keyPair.secretKey,
     
-  })
+  // })
   
-  console.log("tx",tx);
+  // console.log("tx",tx);
 } catch (error) {
-  console.log("error in transfer", error);
+  // console.log("error in transfer", error);
 }
 
 }
