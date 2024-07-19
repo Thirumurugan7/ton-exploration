49944d828a5c357eb9786d24272bd5e92ea128dd
Ovia Seshadri <oviaseshadri@Ovias-MacBook-Air.local>
2024-07-19 12:20:40 +0530

    removed env and node module

--- nativeToken.js
+++ nativeToken.js
@@ -6,6 +6,7 @@ const {
   sign,
   signVerify,
 } = require("@ton/crypto");
+require("dotenv").config()
 
 const tonCenterPoint = "https://toncenter.com";
 const tonApiKey =
@@ -37,33 +38,11 @@ const func = async (recipientAddress) => {
     let mnemonics = await mnemonicNew();
     console.log("Generated mnemonics:", mnemonics);
     // Replace with your mnemonics if needed
-    mnemonics = [
-      'note',
-      'trial',
-      'donor',
-      'decrease',
-      'maze',
-      'coral',
-      'banner',
-      'doll',
-      'promote',
-      'poet',
-      'naive',
-      'earn',
-      'tank',
-      'pass',
-      'gap',
-      'outer',
-      'man',
-      'faint',
-      'option',
-      'best',
-      'neutral',
-      'sausage',
-      'marine',
-      'issue'
-
-    ]
+    let secret = process.env.mnemonics
+    console.log(secret);
+    mnemonics = secret.split(/\s+/);
+    console.log(mnemonics);
+    
 
     // Generating keypair from mnemonics
     let keyPair = await mnemonicToPrivateKey(mnemonics);
